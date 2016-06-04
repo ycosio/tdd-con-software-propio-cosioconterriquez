fdb7b92cdd3aa13985f13e3d0eb44d33ac7f8736
ycosio <ycosio@ucol.mx>
2016-06-03 21:59:31 -0500

    ClaseBurbuja

---
+++ tddparaburbuja/Burbuja.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tddparaburbuja
+{
+    class Burbuja
+    {
+        public void ordenar(int[] arr)
+        {
+            Console.ReadLine();
+            bool cambio = true;
+            while (cambio)
+            {
+                cambio = false;
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        cambio = true;
+                    }
+                }
+            }
+        }
+    }
+}
